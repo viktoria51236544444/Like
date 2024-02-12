2bc96e99c46239ae4da115b703b847160943c888
Bayan Suluu Maatkulova <baiansulu.maatkulova.kg@gmail.com>
2024-02-12 15:07:13 +0600

    wow

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const PostCard = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>HELLO</p>
+      <p>HELLO</p>
+      <p>HELLO</p>
+      <p>HELLO</p>
+      <p>HELLO</p>
+    </div>
+  );
 };
 
 export default PostCard;
